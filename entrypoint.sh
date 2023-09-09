687d391859da03dd4f3ebd8b8dc66a19bbf0c725
Roman Makarevich <remmybump@gmail.com>
2023-09-09 20:34:25 +0300

    fixed config path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 export EXTERNAL_IP=$(curl -H Metadata-Flavor:Google 169.254.169.254/computeMetadata/v1/instance/network-interfaces/?recursive=true -s | jq '.[].accessConfigs[].externalIp' -r)
 
-openssl req -new -newkey rsa:2048 -sha256 -nodes -keyout /tmp/https.key -subj "/CN=$EXTERNAL_IP\/emailAddress=admin@$EXTERNAL_IP/C=US/ST=Ohio/L=Columbus/O=ASAP/OU=NGINX" -out /tmp/https.csr > /dev/null \
-&& openssl req -inform PEM -in /tmp/https.csr -out /tmp/https.pem > /dev/null \
-&& openssl x509 -req -in /tmp/https.pem -signkey /tmp/https.key -out /tmp/https.crt > /dev/null
+openssl req -new -newkey rsa:2048 -sha256 -nodes -keyout /tmp/https.key -subj "/CN=$EXTERNAL_IP\/emailAddress=admin@$EXTERNAL_IP/C=US/ST=Ohio/L=Columbus/O=ASAP/OU=NGINX" -out /tmp/https.csr \
+&& openssl req -inform PEM -in /tmp/https.csr -out /tmp/https.pem \
+&& openssl x509 -req -in /tmp/https.pem -signkey /tmp/https.key -out /tmp/https.crt
 
 mv /tmp/https.{key,crt} /etc/nginx/
 
