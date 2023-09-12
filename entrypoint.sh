1e04efba5950c007cebb60717726de0a0a63c45a
Roman Makarevich <remmybump@gmail.com>
2023-09-12 18:52:25 +0300

    Fixed workflow for initial setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,10 @@ export EXTERNAL_IP=$(curl -H Metadata-Flavor:Google 169.254.169.254/computeMetad
 
 htpasswd -c -b /etc/nginx/.htpasswd $HTUSERNAME $HTPASSWORD 
 
-# using certbot instead of self-signed certificate
-#openssl req -new -newkey rsa:2048 -sha256 -nodes -keyout /tmp/https.key -subj "/CN=$EXTERNAL_IP\/emailAddress=admin@$EXTERNAL_IP/C=US/ST=Ohio/L=Columbus/O=ASAP/OU=NGINX" -out /tmp/https.csr \
-#&& openssl req -inform PEM -in /tmp/https.csr -out /tmp/https.pem \
-#&& openssl x509 -req -in /tmp/https.pem -signkey /tmp/https.key -out /tmp/https.crt
+openssl req -new -newkey rsa:2048 -sha256 -nodes -keyout /tmp/https.key -subj "/CN=$EXTERNAL_IP\/emailAddress=admin@$EXTERNAL_IP/C=US/ST=Ohio/L=Columbus/O=ASAP/OU=NGINX" -out /tmp/https.csr \
+&& openssl req -inform PEM -in /tmp/https.csr -out /tmp/https.pem \
+&& openssl x509 -req -in /tmp/https.pem -signkey /tmp/https.key -out /tmp/https.crt
 
-#mv /tmp/https.{key,crt} /etc/nginx/
+mv /tmp/https.{key,crt} /etc/nginx/
 
 nginx -g 'daemon off;'
