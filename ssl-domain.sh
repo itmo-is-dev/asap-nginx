10ac8c8c8eb36c0803dd60d861bb926fc9f350aa
Roman Makarevich <remmybump@gmail.com>
2023-09-12 20:00:01 +0300

    Added certbot step

--- ssl-domain.sh
+++ ssl-domain.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 DOMAIN="$1"
+FULLCHAIN_FILE_COUNT=$(sudo git ls-files . --exclude-standard --others | grep live/"$DOMAIN"/full | wc -l)
 
-sed -i 's/localhost/'"$DOMAIN"'/g' ./configuration/conf.d/default.conf
-echo "ssl_certificate /etc/nginx/ssl/live/"$DOMAIN"/fullchain.pem;" > ./configuration/shared/"$DOMAIN".conf
-echo "ssl_certificate_key /etc/nginx/ssl/live/"$DOMAIN"/privkey.pem;" >> ./configuration/shared/"$DOMAIN".conf
+if [ $FULLCHAIN_FILE_COUNT -eq 1 ]
+then
+	sed -i 's/localhost/'"$DOMAIN"'/g' ./configuration/conf.d/default.conf
+	echo "ssl_certificate /etc/nginx/ssl/live/"$DOMAIN"/fullchain.pem;" > ./configuration/shared/"$DOMAIN".conf
+	echo "ssl_certificate_key /etc/nginx/ssl/live/"$DOMAIN"/privkey.pem;" >> ./configuration/shared/"$DOMAIN".conf
+	echo "certificate-provider=letsencrypt" >> $GITHUB_OUTPUT
+else
+	echo "certificate-provider=self-signed" >> $GITHUB_OUTPUT
+fi
